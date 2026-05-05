01d194d1ee714d173cbc6209501b092f0d65c510
Lalchand26 <lalchandpahan88@gmail.com>
2026-05-05 17:58:14 +0530

    Add files via upload

---
+++ ecommerce.sql
@@ -0,0 +1,10 @@
+SELECT c.name, p.product_name, od.quantity
+FROM Customers c
+JOIN Orders o ON c.customer_id = o.customer_id
+JOIN OrderDetails od ON o.order_id = od.order_id
+JOIN Products p ON od.product_id = p.product_id;
+
+
+
+
+
